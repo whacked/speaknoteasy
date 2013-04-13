67acc58da263a14cd751ed9114e2d57feceb7d46
natto <natto@natto-tp.(none)>
2013-04-13 18:25:18 -0400

    remove amotoen

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.lithinos/amotoen "0.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.0.1"]
+                 [hiccup "1.0.3"]
+                 [com.cemerick/pomegranate "0.2.0"]
                  ])
